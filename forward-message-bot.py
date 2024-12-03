88768db16687a1a281ed31004415cc13a0dd6321
Kris Stern <kris.stern@gaib.ai>
2024-12-03 17:36:06 +0800

    fix typo and add new script for experimenting

--- forward-message-bot.py
+++ forward-message-bot.py
@@ -45,7 +45,7 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     """Send a message when the command /start is issued."""
     user = update.effective_user
     await update.message.reply_html(
-        rf"Hi {user.mention_html()}! I am the GAIB AI Bog.",
+        rf"Hi {user.mention_html()}! I am the GAIB AI Bot.",
         reply_markup=ForceReply(selective=True),
     )
 
